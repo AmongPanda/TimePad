4b979151ffe0b2648d953637bbe0801ad6f1f6f6
Tema232 <ok-pirozhok@mail.ru>
2023-04-21 02:07:02 +0300

    forms.py

--- timePad/organization/forms.py
+++ timePad/organization/forms.py
@@ -1,15 +1,24 @@
 from django import forms
 from .models import *
+from django.forms.widgets import DateInput
+
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'age', 'date_time', 'buration', 'price', 'description_event', 'mini_description_event',
-                  'image', 'organizer']
+        fields = ['name', 'description', 'date', 'tickets']
+        widgets = {
+            'date': DateInput(attrs={'type': 'date'})
+        }
 
 
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ['name_ticket', 'name_participants', 'description_ticket', 'image_ticket', 'price_ticket',
-                  'email_participants']
+        fields = ['name', 'description', 'price', 'tariff']
+
+
+class UserOrgForm(forms.ModelForm):
+    class Meta:
+        model = UserOrg
+        fields = ['user', 'phone_number', 'adress', 'age_user', 'event']
