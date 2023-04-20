f0c093e2de11011e05f5dcd8defb208e06ec6ecf
Tema232 <ok-pirozhok@mail.ru>
2023-04-21 01:59:58 +0300

    first

---
+++ timePad/organization/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import *
+
+
+class TicketAdmin(admin.ModelAdmin):
+    pass
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+class DiscountAdmin(admin.ModelAdmin):
+    pass
+
+class RegistrationAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserOrgAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(UserOrg, UserOrgAdmin)
